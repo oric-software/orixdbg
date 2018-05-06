e0c48336d8afaa016ceb75a5dbbfc99859990e8b
jede <jede@oric.org>
2018-05-06 21:46:50 +0200

    orixdbg does not work

--- src/orixdbg.c
+++ src/orixdbg.c
@@ -8,9 +8,87 @@ extern int rs232(void);
 extern void rs232_console();
 extern void send_byte_rs232();
 
+#define ACIA_TXE	 0x10
+
+
+#define DATA_6551_REGISTER 0x31C
+#define STATUS_6551_REGISTER 0x31D
+#define COMMAND_6551_REGISTER 0x31E
+#define CONTROL_6551_REGISTER 0x31F
+
+void jasmin_menu()
+{
+}
+
+void joystick_menu()
+{
+	unsigned char control;
+	unsigned char control_button;
+	printf("Press any joysticks\n");
+	POKE(0x0322,128+64);
+	POKE(0x0320,128+64);
+	while (1)
+	{
+		control=PEEK(0x320)&(1+2+4+8+16);
+		control_button=PEEK(0x321);
+		printf("control %d\n",control);
+		if (control&1==0)
+			printf("Right\n");
+		if (control&2==0)
+			printf("Left\n");		
+		if (control&4==0)
+			printf("fire\n");		
+		if (control&8==0)
+			printf("down\n");
+		if (control&16==0)
+			printf("up\n");
+		if (control_button&32==0)
+		    printf("third button\n");
+		if (control_button&128==0)
+		    printf("Second button\n");		
+		}
+}
+
+
+void wait()
+{
+	unsigned char i;
+	for (i=0;i<0x68;i++);
+}
+
+
+void send_byte(unsigned char car)
+{
+	while((PEEK(STATUS_6551_REGISTER)&ACIA_TXE)!=ACIA_TXE)
+	{
+		printf("Waiting ...\n");
+	}
+	printf("Sending : %d\n",car);
+	POKE(DATA_6551_REGISTER,car);
+	wait();
+}
+
+void setcommand_rs232()
+{
+  printf("Setting control register\n");
+  printf("* Set 9600 bauds\n"); // 
+  printf("* 1 stop bit\n"); // 
+  printf("* Word length 8\n"); 
+  printf("* Baud rate clock\n"); // 16
+  
+  POKE(CONTROL_6551_REGISTER,0+2+4+8+16);
+  
+  printf("Setting command register\n");
+  printf("* Disable IRQ\n");
+  printf("* DTR ready\n");
+  printf("* transmitter intrrupt control : do not use\n");
+  POKE(COMMAND_6551_REGISTER,1+2+4+8);
+
+}
+
 void print_6551_status_register(unsigned char       status)
 {
-   printf("Status   : 0x%x\n",status);
+  printf("Status   : 0x%x\n",status);
   printf("      b7 (irq)     : 0x%x\n",status&128);
   printf("      b6 (dsrb)    : 0x%x\n",status&64);
   printf("      b5 (dcdb)    : 0x%x\n",status&32);
@@ -35,31 +113,41 @@ void rs232_menu()
     clrscr();
     printf("Read RS232 input and displays .. 1\n");
     printf("Output ACIA registers         .. 2\n");
-    printf("Send 'A' 200 times            .. 3\n");
+    printf("Send 'A' 20 times             .. 3\n");
     printf("Start console                 .. 4\n");
-    printf("Quit menu                     .. 5\n");
+    printf("Set command register          .. 5\n");
+    printf("Quit menu                     .. 6\n");
     key=cgetc();
     if (key=='2')
     {
-      data=PEEK(0x31C);
-      status=PEEK(0x31D);
-      cmd=PEEK(0x31E);
-      ctrl=PEEK(0x31F);
+      data=PEEK(DATA_6551_REGISTER);
+      status=PEEK(STATUS_6551_REGISTER);
+      cmd=PEEK(COMMAND_6551_REGISTER);
+      ctrl=PEEK(CONTROL_6551_REGISTER);
       print_6551_status_register(status);
        
       printf("Control : 0x%x\n",ctrl);
       printf("Cmd : 0x%x\n",cmd);
-      printf("data : 0x%x\n",ctrl);
+      printf("data : 0x%x\n",data);
     }
     if (key=='3')
     {
+      setcommand_rs232();
+	  
+	  status=PEEK(STATUS_6551_REGISTER);
+      print_6551_status_register(status);
+	  
       for(i=0;i<20;i++)
       {
         printf("%d .. ",i);
-        status=PEEK(0x31D);
-        print_6551_status_register(status);
+        //POKE(DATA_6551_REGISTER,0x42);
+		
+		send_byte('A');  
+		print_6551_status_register(status);
         send_byte_rs232();
+		print_6551_status_register(status);
       }
+
       printf("\n");
     }
     if (key=='4')
@@ -69,42 +157,57 @@ void rs232_menu()
     
     if (key=='5')
     {
+      setcommand_rs232();
+    }
+        
+    
+    if (key=='6')
+    {
       break;
     }
     printf("Press a key ... \n");
     key=cgetc();
   }
 }
-/*
-#define ACIADR $031C ; DATA REGISTER
-#define ACIASR $031D ; STATUS REGISTER
-#define ACIACR $031E ; command register
-#define ACIACT $031F ; control register
 
-
-*/
 
 int main ()
 {
   unsigned char key;
   clrscr();
-  //textcolor(COLOR_RED);
-  //cputc('b');
-  //puts("fff");
-  cvline (5);
-  //gotoxy (10,10);
-  //cprintf("Hello");
-  printf("Orixdbg\n");
+
+  cputc(CH_ULCORNER);
+  chline(38);
+  cputc(CH_URCORNER);
+  printf("|                 Orixdbg              |");
+  cputc(CH_ULCORNER);
+  chline(38);
+  cputc(CH_URCORNER);  
   textcolor(COLOR_GREEN);
-  printf("Start rs232 tool .. 1\n");  
-  printf("Quit             .. 2\n");
+  printf("|Start rs232 tool                    |1|"); 
+  cputc(CH_ULCORNER);
+  chline(38);
+  textcolor(COLOR_GREEN);
+  cputc(CH_ULCORNER);
+  printf("|Start joystick tool                 |2|"); 
+  cputc(CH_ULCORNER);
+  chline(38);
+  
+  cputc(CH_URCORNER);  
+  printf("|Quit                                |3|");
+  cputc(CH_ULCORNER);
+  chline(38);
+  cputc(CH_URCORNER);
   while (1)
   {
     key=cgetc();
     if (key=='1')
       rs232_menu();
+    if (key=='2')
+      joystick_menu();
+  
     
-    if (key==27 ||key=='2')
+    if (key==27 ||key=='3')
       break;
   }
     //rs232();
--- src/rs232.asm
+++ src/rs232.asm
@@ -2,37 +2,6 @@
 #include "../orix/src/include/macro.h"
 #include "../orix/src/include/6551.h"
 #include "../orix/src/include/keyboard.h"
-/*
-    *=$1000-20
-; include header
-  .byt $01,$00	      	; non-C64 marker like o65 format
-  .byt "o", "r", "i"      ; "ori" MAGIC number :$6f, $36, $35 like o65 format
-  .byt $01                ; version of this header
-cpu_mode
-  .byt $00                ; CPU see below for description
-language_type
-  .byt $00	             ; reserved in the future, it will define if it's a Hyperbasic file, teleass file, forth file 
-  .byt $00                ; reserved
-  .byt $00		            ; reserved
-  .byt $00	              ; operating system id for telemon $00 means telemon 3.0 version
-  .byt $00	              ; reserved
-	.byt $00                ; reserved
-type_of_file
-	.byt %01001001                   ; Auto, direct, data for stratsed, sedoric, ftdos compatibility
-	.byt <start_adress,>start_adress ; loading adress
-	.byt <EndOfMemory,>EndOfMemory   ; end of loading adress
-	.byt <start_adress,>start_adress ; starting adress
- 
-start_adress
- 
-	*=$1000
-  */
-/*  
-#define ACIADR $031C
-#define ACIASR $031D ; STATUS REGISTER
-#define ACIACR $031E ; control register
-#define ACIACT $031F
-*/
 
 
 
